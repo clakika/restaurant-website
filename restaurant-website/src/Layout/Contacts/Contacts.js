4f6ebc101b8b2b8be0c099760a6865f07cb03bba
clakika <31564756+clakika@users.noreply.github.com>
2020-07-28 14:55:18 +0200

    contacts finished
    icons to be added

--- restaurant-website/src/Layout/Contacts/Contacts.js
+++ restaurant-website/src/Layout/Contacts/Contacts.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Contacts.css';
 
 import contactsImage from '../../img/contacts-image.png';
 
 export class Contacts extends Component {
     render() {
         return (
-            <div>
-                <h2>share the love for pizza</h2>
-                <div>
-                    <p>we are on all social media platforms!visit our socials and share your experience!</p>
+            <div className="Contacts_Section">
+                <h2 className="Contacts_Title">share the love for pizza</h2>
+                <img className="Contacts_Image" src={contactsImage}></img>
+                <div className="Contacts_Socials">
+                    <p className="Contacts_SubText">we are on all social media platforms!<br></br>visit our socials and share your experience!</p>
+                    <i className="fab fa-instagram"></i>
                     <i>O</i>
                     <i>O</i>
                     <i>O</i>
-                    <i>O</i>
-                    <h4>any questions?</h4>
+                    <h4 className="Contacts_Questions">any questions?</h4>
                     <p><i>O</i>amoreperlapizza@italia.it</p>
                     <p><i>O</i>+123 456 789</p>
                 </div>
-                <img src={contactsImage}></img>
-                <footer> &copy; 2020 Claudia Kiril Cappai</footer>
+                <footer className="Copyright"> &copy; 2020 Claudia Kiril Cappai</footer>
             </div>
         )
     }
