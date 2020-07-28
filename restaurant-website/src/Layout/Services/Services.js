1c3cf21dcd15a70e06220f3818b818403cf8c333
clakika <31564756+clakika@users.noreply.github.com>
2020-07-28 11:58:28 +0200

    services section finished

--- restaurant-website/src/Layout/Services/Services.js
+++ restaurant-website/src/Layout/Services/Services.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import './Services.css'
 
 import serviceImgOne from '../../img/services-image1.png';
 import serviceImgTwo from '../../img/services-image2.png';
+
 
 
 export class Services extends Component {
     render() {
         return (
             <div>
-                <h2>enjoy your pizza wherever you want</h2>
-                <p>visit our cozy restaurant in the heart of Berlin, or simply take your pizza to your most special places.
-                <a href="#">Discover our take-away and delivery services</a></p>
-                <img src={serviceImgOne}></img>
-                <img src={serviceImgTwo}></img>
+                <div className="Services_Container">
+                    <div className="Services_Text">
+                        <h2 className="Services_Title">enjoy your pizza wherever you want</h2>
+                        <p className="Services_Subtext">visit our cozy restaurant in the heart of Berlin, or simply take your pizza to your most special places.
+                        <br></br><a className="Services_Link" href="#">Discover our take-away and delivery services</a></p>
+                    </div>
+                    <img className="Services_Image Services_ImageOne" src={serviceImgOne}></img>
+                    <img className="Services_Image Services_ImageTwo" src={serviceImgTwo}></img>
+                </div>
             </div>
         )
     }
