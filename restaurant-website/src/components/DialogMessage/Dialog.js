30414b2218d473bc914ae32f4c557c011d9c92ca
clakika <31564756+clakika@users.noreply.github.com>
2020-08-03 15:10:25 +0200

    reservation part finished

--- restaurant-website/src/components/DialogMessage/Dialog.js
+++ restaurant-website/src/components/DialogMessage/Dialog.js
@@ -1,51 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Dialog.css';
 
 export class Dialog extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            display: 'none'
-        }
-
-        this.state = {
-            OverlayMessageBooking: {
-                opacity: '0.8',
-                backgroundColor: '#ccc',
-                position: 'fixed',
-                width: '100%',
-                height: '100%',
-                top: '0px',
-                left: '0px',
-                display: 'none',
-            },
-
-            BookedMessage: {
-                backgroundColor: 'whitesmoke',
-                opacity: '0.9',
-                width: '30vw',
-                height: '16vw',
-                padding: '1.5rem',
-                margin: '0 auto',
-                position: 'absolute',
-                transform: 'translate(15rem, 10rem)',
-                border: '3px solid rgb(146, 43, 33)',
-                display: 'none',
-            }
-        }
-
-    };
-
     render() {
         return (
-            <div>
-                <div style={this.state.OverlayMessageBooking}></div>
-                    <div style={this.state.BookedMessage}>
-                        <p className="BookedMessage_Text">Thank you for your reservation!<br></br>Shortly you will receive a confirmation email.<br></br>See you soon! :)</p>
-                </div>
+            <div className="BookingMessage">
+                <p>Thank you for your reservation! See you soon!</p> 
             </div>
         )
     }
 }
 
-export default Dialog
+export default Dialog;
--- restaurant-website/src/components/Form/Form.js
+++ restaurant-website/src/components/Form/Form.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
 import './Form.css';
+import Dialog from '../../components/DialogMessage/Dialog';
 
-const emailRegex = RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
-const formValid = ({ formErrors, ...rest }) => {
-    let valid = true;
-
-    Object.values(formErrors).forEach( val => 
-        {val.length > 0 && (valid = false)});
-
-    Object.values(rest).forEach(val => {
-        val === null && (valid = false);
-    });
-    
-    return valid;
-};
-
+/*const emailRegex = RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);*/
 
 export class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
             disabled: true,
+            visible: false,
             firstName: "",
             lastName: "",
             date: "",
@@ -29,49 +18,16 @@ export class Form extends Component {
             partySize: 0,
             email: "",
             phoneNumber: "",
-            notes: "",
-            formErrors: {
-                firstName: "",
-                lastName: "",
-                email: "",
-                phoneNumber: ""},
-            BookingMessage: {
-                fontSize: '17px',
-                marginTop: '2rem',
-                display: 'none'}
+            notes: ""
     }
 };
 
     
     handleChange = e => {
         e.preventDefault();
-        const { name, value } = e.target;
-        /*this.setState({
+        this.setState({
             [e.target.name]: e.target.value
-        });*/
-        let formErrors = { ...this.state.formErrors };
-
-        switch (name) {
-            case 'firstName':
-                formErrors.firstName = value.length < 3  ? "minimum 3 characters required" : "✓";
-                break;
-
-            case 'lastName':
-                formErrors.lastName = value.length < 3 ? "minimum 3 characters required" : "✓";
-                break;
-
-            case 'email':
-                formErrors.email = emailRegex.test(value) ? "✓" : "invalid email address";
-                break;
-
-            case 'phoneNumber':
-                formErrors.phoneNumber = value.length < 15 ? "only numbers required" : "✓";
-                break;
-            default:
-            break;
-        }
-
-        this.setState({ formErrors, [name]: value}, () => console.log(this.state));
+        });
     };
 
     isValid = () => {
@@ -108,62 +64,46 @@ export class Form extends Component {
     }
 
     onClick = (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
 
-        let BookingMessage = this.state.BookingMessage;
-
-        this.setState({
-            BookingMessage: {
-                ...BookingMessage,
-                display: 'block'
-            }
-        });
+        this.setState({ BookingMessage: { ...this.state.BookingMessage, display: "block"} });
     }
 
     render() {
-        const { formErrors } = this.state;
 
         return (
             <div>
-                <form className="Reservation_Form" onSubmit={this.handleSubmit} onFocus={this.ValueChecker} noValidate>
+                <form className="Reservation_Form" onSubmit={this.handleSubmit} onFocus={this.ValueChecker}>
                 <label>first name<span className="Asteriks">*</span></label>
                 <input 
-                    className={formErrors.firstName.length > 0 ? "error" : null}
                     type="text" 
-                    name="firstName"
-                    noValidate 
+                    name="firstName" 
                     onChange={this.handleChange} 
+                    required
                      />
-                     {formErrors.firstName.length > 0 && (
-                    <span className="errorMessageFirstName">{formErrors.email}</span>
-                    )}
                 <br />
                 <label>last name<span className="Asteriks">*</span></label>
                 <input 
-                    className={formErrors.lastName.length > 0 ? "error" : null}
                     type="text" 
                     name="lastName"
-                    noValidate
-                    onChange={this.handleChange} 
+                    onChange={this.handleChange}
+                    required 
                      />
-                     {formErrors.lastName.length > 0 && (
-                    <span className="errorMessageLastName">{formErrors.email}</span>
-                    )}
                 <br />
                 <label>date<span className="Asteriks">*</span></label>
                 <input 
                     type="date" 
                     name="date"
-                    noValidate
                     onChange={this.handleChange} 
+                    required
                      />
                 <br />
                 <label>time<span className="Asteriks">*</span></label>
                 <input 
                     type="time" 
                     name="time"
-                    noValidate
                     onChange={this.handleChange} 
+                    required
                     />
                 <br />
                 <label>party size<span className="Asteriks">*</span></label>
@@ -172,46 +112,35 @@ export class Form extends Component {
                     min="1" 
                     max="30" 
                     name="partySize"
-                    noValidate 
                     onChange={this.handleChange} 
+                    required
                      />
                 <br />
                 <label>email</label>
                 <input 
                     type="email" 
                     name="email"
-                    noValidate
-                    className={formErrors.email.length > 0 ? "error" : null}
                     onChange={this.handleChange} 
+                    required
                     />
-                    {formErrors.email.length > 0 && (
-                    <span className="errorMessageEmail">{formErrors.email}</span>
-                    )}
                 <br />  
                 <label>phone number<span className="Asteriks">*</span></label>
                 <input 
                     type="tel" 
                     name="phoneNumber"
-                    noValidate
-                    className={formErrors.phoneNumber.length > 0 ? "error" : null}
                     onChange={this.handleChange} 
+                    required
                     />
-                    {formErrors.phoneNumber.length > 0 && (
-                    <span className="errorMessagePhoneNumber">{formErrors.email}</span>
-                    )}
                 <br />
                 <label>notes on reservation</label>
                 <textarea 
                     type="text" 
                     name="notes"  
-                    noValidate
                     onChange={this.handleChange} />
                 <br />
-                <button type="button" className="BookBtn" disabled={this.state.disabled}>book</button>
+                <button type="button" className="BookBtn" onClick={() => {this.setState({visible: true});}} disabled={this.state.disabled}>book</button>
+                {this.state.visible ? <Dialog /> : null}
             </form> 
-            <div className={this.state.BookingMessage}>
-                    <p>Thank you for your reservation! See you soon!</p> 
-            </div>
         </div>
         )
     };
